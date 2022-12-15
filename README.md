bf2e4d003388235ceafcb6539d9de888bc51cd66
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2022-12-15 11:29:02 -0800

    - Add an aggregated graph (data rate by month). - Add latency graph. - Refactor rrd_utils.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-## Initial setup
+This repo contains analytics performed on metrics we gather via SNMP.
+
+```
+LibreNMS --> rrd databases --> rrdtool fetch --> python graphing libraries
+```
+
+Essentially, we extract data from RRD databases, and plot using not `rrdtool` but mainstream graphing libraries.
+
+## First-time setup
 
 Run this command. This ensures that we don't commit notebooks' outputs.
 
@@ -6,23 +14,26 @@ Run this command. This ensures that we don't commit notebooks' outputs.
 git config --local include.path ../.gitconfig
 ```
 
+Install jupyter notebook on the host where you'll be executing `git commit`:
+
+1. `pip install notebook`
+    (This installs a notebook globally to your python installation. If you prefer to isolate its installation to this project, see instructions on `venv` below.)
+
 ## Runing the notebook server
 
-Choose one of below options.
-
-### Running in Docker
+### Option A: Running in Docker
 
 Bringup:
 1. `docker compose up --build -d`
-1. Optionally, if you want to use a shell inside the container, then
+1. Optionally, to use a shell inside the container (without going through a notebook), then
     `docker compose exec -it main bash`
 
 Teardown:
 1. `docker compose down`
 
-### Running on Linux without Docker
+### Option B: Running without Docker
 
-Setup:
+First-time setup:
 1. [Install pyenv](https://github.com/pyenv/pyenv#installation)
 1. `python -m venv ./venv`
 
@@ -34,7 +45,9 @@ Teardown:
 1. Kill jupyter notebook: `kill %1`
 1. `deactivate`
 
-### Running on Colab
+Caveat: On MacOS, it is unknown whether `brew rrdtool` installs the headers required by python package `rrdtool`. For consistency, MacOS users are recommended to use Docker.
+
+### Option C: Running on Colab
 
 Running on Colab is not supported, for now. To run there, we should
 - dedicate a Google Drive to storing RRD files as their snapshots
@@ -42,3 +55,7 @@ Running on Colab is not supported, for now. To run there, we should
 
 1. Go to https://colab.research.google.com/github/
 1. Enter the github url of this repo.
+
+## Accessing the notebook server
+
+http://localhost:8888/
--- scn_rrd/rrd_utils.py
+++ scn_rrd/rrd_utils.py
@@ -1,47 +1,54 @@
 ## Stdlib
+from enum import StrEnum
 import os
+from typing import Dict
 from types import SimpleNamespace
 
 ## Non-std libs
 import pandas as pd
 import rrdtool
 
-RRD_DIRPATH_PROD = '/opt/librenms/rrd'
-RRD_DIRPATH_LOCAL = './data'
+class RrdDirpath(StrEnum):
+    PROD = '/opt/librenms/rrd'
+    LOCAL = './data'
 
 ## site :: nickname :: rrd relative file path
 RRD_RELPATHS = {
     'fcs': SimpleNamespace(
         uplink='fcs1/port-id1874.rrd',
         avail_weekly='fcs1/availability-2592000.rrd',
+        ping='fcs1/ping-perf.rrd',
     ),
     'garfield': SimpleNamespace(
         uplink='sps-garfield/port-id190.rrd',
         avail_weekly='sps-garfield/availability-2592000.rrd',
+        ping='sps-garfield/ping-perf.rrd',
     ),
     'occ': SimpleNamespace(
         uplink='scn-occ/port-id5175.rrd',
         avail_weekly='scn-occ/availability-2592000.rrd',
+        ping='scn-occ/ping-perf.rrd',
     ),
     'tcn_surge': SimpleNamespace(
         uplink='tcn-surge/port-id507.rrd',
         avail_weekly='tcn-surge/availability-2592000.rrd',
+        ping='tcn-surge/ping-perf.rrd',
     )
 }
 
-def rrd_path(relpath: str, dirpath: str = RRD_DIRPATH_LOCAL) -> str:
+def rrd_path(dirpath: RrdDirpath, relpath: str) -> str:
     return f'{dirpath}/{relpath}'
 
-def rrd_to_pandas(rrd_relpath: str, start_time: str, end_time: str = None) -> pd.DataFrame:
+def rrd_to_dataframe(rrd_relpath: str, start_time: str, end_time: str = None) -> pd.DataFrame:
     '''
     @arg rrd_relpath: RRD_RELPATHS.foo.bar
     @arg start_time and end_time: https://oss.oetiker.ch/rrdtool/doc/rrdfetch.en.html#AT-STYLE_TIME_SPECIFICATION
-        end_time defaults to RRD's last received update.
+        end_time defaults to the time of RRD's last received update.
     '''
-    rrd_fullpath = rrd_path(rrd_relpath)
-    info = rrdtool.info(rrd_fullpath)
+    rrd_fullpath = rrd_path(RrdDirpath.LOCAL, rrd_relpath)
 
     if end_time is None:
+        info = rrdtool.info(rrd_fullpath)
         end_time = str(info['last_update'])
 
     ((start, end, step), ds, rows) = rrdtool.fetch(
@@ -49,11 +56,20 @@ def rrd_to_pandas(rrd_relpath: str, start_time: str, end_time: str = None) -> pd
         '--start', start_time,
         '--end', end_time)
 
-    ts = range(start, end, step)
-
+    ts = range(start, end, step)    # `end` is exclusive.
     assert len(ts) == len(rows)
 
-    ret_cols = ('time',) + ds
-    ret_rows = ((t,) + row for (t, row) in zip(ts, rows))
-    df = pd.DataFrame(ret_rows, columns=ret_cols)
+    ts = pd.to_datetime(ts, unit='s')
+    df_cols = ('time',) + ds
+    df_rows = ((t,) + row for (t, row) in zip(ts, rows))
+    df = pd.DataFrame(data=df_rows, columns=df_cols)
     return df
+
+def collect_rrd_dataframes(nickname: str, start_time: str, end_time: str = None) -> Dict[str, pd.DataFrame]:
+    site_to_df = dict()
+    for (site, nickname_to_relpath) in RRD_RELPATHS.items():
+        rrd_relpath = getattr(nickname_to_relpath, nickname)
+        if rrd_relpath is not None:
+            df = rrd_to_dataframe(rrd_relpath, start_time, end_time)
+            site_to_df[site] = df
+    return site_to_df
